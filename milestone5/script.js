8c5aef87eefa4c075dea92fdc77179642f16e5e4
FizaSohail1 <fizasohail512@gmail.com>
2024-09-22 04:31:13 +0500

    milestone5

---
+++ milestone5/script.js
@@ -0,0 +1,116 @@
+var html2pdf;
+var skillbtn = document.getElementById("addmoreSkills");
+var SkillsList = document.querySelector(".skills-list");
+var ResumeForm = document.getElementById('resumeForm');
+var ResumeDisplay = document.getElementById('resumeDisplay');
+var shareableLinkContainer = document.getElementById('shareContainer');
+var shareLink = document.getElementById('shareLink');
+var downloadPDF = document.getElementById('downloadPDF');
+var mainHeading = document.getElementById('mainHeading');
+var shareHeading = document.getElementById('shareHeading');
+skillbtn.onclick = function () {
+    var newField = document.createElement('input');
+    newField.setAttribute('type', 'text');
+    newField.setAttribute('name', 'skills-list[]');
+    SkillsList.appendChild(newField);
+};
+var ImageUrl = '';
+var ProfileImage = document.getElementById('img');
+var ImagePreview = document.getElementById('image');
+ProfileImage.addEventListener('change', function () {
+    var _a;
+    var PImage = (_a = ProfileImage.files) === null || _a === void 0 ? void 0 : _a[0];
+    if (PImage) {
+        var reader_1 = new FileReader();
+        reader_1.onloadend = function () {
+            ImageUrl = reader_1.result;
+        };
+        reader_1.readAsDataURL(PImage);
+    }
+});
+ResumeForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var username = document.getElementById('userName').value.trim();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var contact = document.getElementById('contact').value;
+    var about = document.getElementById('about').value;
+    var degree = document.getElementById('degree').value;
+    var academicYear = document.getElementById('academicYear').value;
+    var school = document.getElementById('school').value;
+    var company = document.getElementById('company').value;
+    var position = document.getElementById('position').value;
+    var startDate = document.getElementById('startDate').value;
+    var endDate = document.getElementById('endDate').value;
+    var description = document.getElementById('experienceDescription').value.trim();
+    var skillInputs = document.querySelectorAll('.skills-list input');
+    var skills = Array.from(skillInputs).map(function (skillInput) { return skillInput.value.trim(); }).filter(function (skill) { return skill !== ''; });
+    var resumeHTML = "\n        <div class=\"card\">\n          <div class=\"personalInfo\">\n            <img src=\"".concat(ImageUrl, "\" alt=\"Profile Image\" height=\"200px\" width=\"200px\" id=\"image\"/>\n            <h2>Personal Information</h2>\n            <p><strong>Name:</strong> ").concat(name, "</p>\n            <p><strong>Contact:</strong> ").concat(contact, "</p>\n            <p><strong>Email:</strong> ").concat(email, "</p>\n            <div class=\"about\">\n              <h3>About</h3>\n              <p>").concat(about, "</p>\n            </div>\n          </div> \n          <div class=\"secondSection\">\n            <div class=\"experienceDisplay\">\n              <h2>Work Experience</h2>\n              <p><strong>Company:</strong> ").concat(company, "</p>\n              <p><strong>Position:</strong> ").concat(position, "</p>\n              <p><strong>From:</strong> ").concat(startDate, "</p>\n              <p><strong>To:</strong> ").concat(endDate, "</p>\n              <p><strong>Description:</strong> ").concat(description, "</p>\n            </div>\n            <section class=\"skills-container\">\n              <h2>Skills</h2>\n              <ul class=\"skills-list\">\n                ").concat(skills.map(function (skill) { return "<li>".concat(skill, "</li>"); }).join(''), "  \n              </ul>\n            </section>\n            <h2>Education</h2>\n            <p><strong>Degree:</strong> ").concat(degree, "</p>\n            <p><strong>Year:</strong> ").concat(academicYear, "</p>\n            <p><strong>School/College:</strong> ").concat(school, "</p>\n          </div>\n        </div>\n    ");
+    var resumeData = {
+        name: name,
+        email: email,
+        contact: contact,
+        about: about,
+        company: company,
+        position: position,
+        startDate: startDate,
+        endDate: endDate,
+        description: description,
+        degree: degree,
+        academicYear: academicYear,
+        school: school,
+        skills: skills,
+        ImageUrl: ImageUrl
+    };
+    localStorage.setItem(username, JSON.stringify(resumeData));
+    var shareableURL = "".concat(window.location.origin, "/milestone5/index.html?username=").concat(encodeURIComponent(username));
+    shareLink.href = shareableURL;
+    shareableLinkContainer.style.display = 'block';
+    shareLink.textContent = shareableURL;
+    if (ResumeDisplay) {
+        ResumeDisplay.innerHTML = resumeHTML;
+    }
+});
+window.addEventListener('DOMContentLoaded', function () {
+    var urlParams = new URLSearchParams(window.location.search);
+    var username = urlParams.get('username');
+    if (username) {
+        ResumeForm.style.display = 'none';
+        mainHeading.style.display = 'none';
+        var savedResumeData = localStorage.getItem(username);
+        if (savedResumeData) {
+            var resumeData = JSON.parse(savedResumeData);
+            var resumeHTML = "\n                <div class=\"card\">\n                  <div class=\"personalInfo\">\n                    <img src=\"".concat(resumeData.ImageUrl, "\" alt=\"Profile Image\" height=\"200px\" width=\"200px\" id=\"image\"/>\n                    <h2>Personal Information</h2>\n                    <p><strong>Name:</strong> ").concat(resumeData.name, "</p>\n                    <p><strong>Contact:</strong> ").concat(resumeData.contact, "</p>\n                    <p><strong>Email:</strong> ").concat(resumeData.email, "</p>\n                    <div class=\"about\">\n                      <h3>About</h3>\n                      <p>").concat(resumeData.about, "</p>\n                    </div>\n                  </div>\n                  <div class=\"secondSection\">\n                    <div class=\"experienceDisplay\">\n                      <h2>Work Experience</h2>\n                      <p><strong>Company:</strong> ").concat(resumeData.company, "</p>\n                      <p><strong>Position:</strong> ").concat(resumeData.position, "</p>\n                      <p><strong>From:</strong> ").concat(resumeData.startDate, "</p>\n                      <p><strong>To:</strong> ").concat(resumeData.endDate, "</p>\n                      <p><strong>Description:</strong> ").concat(resumeData.description, "</p>\n                    </div>\n                    <section class=\"skills-container\">\n                      <h2>Skills</h2>\n                      <ul class=\"skills-list\">\n                        ").concat(resumeData.skills.map(function (skill) { return "<li>".concat(skill, "</li>"); }).join(''), "  \n                      </ul>\n                    </section>\n                    <h2>Education</h2>\n                    <p><strong>Degree:</strong> ").concat(resumeData.degree, "</p>\n                    <p><strong>Year:</strong> ").concat(resumeData.academicYear, "</p>\n                    <p><strong>School/College:</strong> ").concat(resumeData.school, "</p>\n                  </div>\n                </div>\n            ");
+            ResumeDisplay.innerHTML = resumeHTML;
+        }
+    }
+});
+var GenerateButton = document.getElementById('generateButton');
+var saveButton = document.getElementById('saveButton');
+var generated = false;
+GenerateButton.addEventListener('click', function () {
+    if (!generated) {
+        GenerateButton.textContent = "Edit Resume";
+        saveButton.style.display = 'inline-block';
+        generated = true;
+        shareHeading.style.display = 'flex';
+        shareLink.style.display = 'block';
+    }
+});
+saveButton.addEventListener('click', function () {
+    ResumeForm.style.display = 'none';
+    mainHeading.style.display = 'none';
+    downloadPDF.style.display = 'block';
+    alert('Resume Saved Successfully');
+});
+// download PDF
+downloadPDF.addEventListener('click', function () {
+    if (ResumeDisplay && ResumeDisplay.innerHTML) {
+        var opt = {
+            filename: 'resume.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        };
+        html2pdf().from(ResumeDisplay).set(opt).save();
+    }
+});
---
+++ milestone5/script.ts
@@ -0,0 +1,202 @@
+let html2pdf: any;
+
+const skillbtn = document.getElementById("addmoreSkills") as HTMLButtonElement;
+const SkillsList = document.querySelector(".skills-list") as HTMLDivElement;
+const ResumeForm = document.getElementById('resumeForm') as HTMLFormElement;
+const ResumeDisplay = document.getElementById('resumeDisplay') as HTMLDivElement;
+const shareableLinkContainer = document.getElementById('shareContainer') as HTMLDivElement;
+const shareLink = document.getElementById('shareLink') as HTMLAnchorElement;
+const downloadPDF = document.getElementById('downloadPDF') as HTMLButtonElement;
+const mainHeading = document.getElementById('mainHeading') as HTMLHeadingElement;
+const shareHeading = document.getElementById('shareHeading') as HTMLHeadingElement;
+
+skillbtn.onclick = function () {
+    const newField = document.createElement('input') as HTMLInputElement;
+    newField.setAttribute('type', 'text');
+    newField.setAttribute('name', 'skills-list[]');
+    SkillsList.appendChild(newField);
+};
+
+
+let ImageUrl:string = '';
+const ProfileImage = document.getElementById('img') as HTMLInputElement;
+const ImagePreview = document.getElementById('image') as HTMLImageElement
+
+ProfileImage.addEventListener('change',() =>{
+const PImage = ProfileImage.files?.[0];
+if(PImage){
+  const reader = new FileReader();
+  reader.onloadend = () => {
+    ImageUrl = reader.result as string
+  }
+  reader.readAsDataURL(PImage)
+}
+})
+ResumeForm.addEventListener('submit', (event: Event) => {
+    event.preventDefault();
+
+    const username = (document.getElementById('userName') as HTMLInputElement).value.trim();
+    const name = (document.getElementById('name') as HTMLInputElement).value;
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const contact = (document.getElementById('contact') as HTMLInputElement).value;
+    const about = (document.getElementById('about') as HTMLTextAreaElement).value;
+    const degree = (document.getElementById('degree') as HTMLInputElement).value;
+    const academicYear = (document.getElementById('academicYear') as HTMLInputElement).value;
+    const school = (document.getElementById('school') as HTMLInputElement).value
+    const company = (document.getElementById('company') as HTMLInputElement).value;
+    const position = (document.getElementById('position') as HTMLInputElement).value;
+    const startDate = (document.getElementById('startDate') as HTMLInputElement).value;
+    const endDate = (document.getElementById('endDate') as HTMLInputElement).value
+    const description = (document.getElementById('experienceDescription') as HTMLTextAreaElement).value.trim();
+    const skillInputs = document.querySelectorAll('.skills-list input') as NodeListOf<HTMLInputElement>;
+    const skills = Array.from(skillInputs).map(skillInput => skillInput.value.trim()).filter(skill => skill !== '');
+
+    const resumeHTML = `
+        <div class="card">
+          <div class="personalInfo">
+            <img src="${ImageUrl}" alt="Profile Image" height="200px" width="200px" id="image"/>
+            <h2>Personal Information</h2>
+            <p><strong>Name:</strong> ${name}</p>
+            <p><strong>Contact:</strong> ${contact}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <div class="about">
+              <h3>About</h3>
+              <p>${about}</p>
+            </div>
+          </div> 
+          <div class="secondSection">
+            <div class="experienceDisplay">
+              <h2>Work Experience</h2>
+              <p><strong>Company:</strong> ${company}</p>
+              <p><strong>Position:</strong> ${position}</p>
+              <p><strong>From:</strong> ${startDate}</p>
+              <p><strong>To:</strong> ${endDate}</p>
+              <p><strong>Description:</strong> ${description}</p>
+            </div>
+            <section class="skills-container">
+              <h2>Skills</h2>
+              <ul class="skills-list">
+                ${skills.map(skill => `<li>${skill}</li>`).join('')}  
+              </ul>
+            </section>
+            <h2>Education</h2>
+            <p><strong>Degree:</strong> ${degree}</p>
+            <p><strong>Year:</strong> ${academicYear}</p>
+            <p><strong>School/College:</strong> ${school}</p>
+          </div>
+        </div>
+    `;  
+    const resumeData = {
+      name,
+      email,
+      contact,
+      about,
+      company,
+      position,
+      startDate,
+      endDate,
+      description,
+      degree,
+      academicYear,
+      school,
+      skills,
+      ImageUrl 
+  };
+  localStorage.setItem(username, JSON.stringify(resumeData));
+
+    const shareableURL = `${window.location.origin}/milestone5/index.html?username=${encodeURIComponent(username)}`
+    shareLink.href = shareableURL;
+    shareableLinkContainer.style.display = 'block';
+    shareLink.textContent = shareableURL;
+
+    if (ResumeDisplay) {
+      ResumeDisplay.innerHTML = resumeHTML;
+  }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const username = urlParams.get('username');
+
+    if (username) {
+        ResumeForm.style.display = 'none';
+        mainHeading.style.display = 'none';
+
+        const savedResumeData = localStorage.getItem(username);
+        if (savedResumeData) {
+            const resumeData = JSON.parse(savedResumeData);
+
+            const resumeHTML = `
+                <div class="card">
+                  <div class="personalInfo">
+                    <img src="${resumeData.ImageUrl}" alt="Profile Image" height="200px" width="200px" id="image"/>
+                    <h2>Personal Information</h2>
+                    <p><strong>Name:</strong> ${resumeData.name}</p>
+                    <p><strong>Contact:</strong> ${resumeData.contact}</p>
+                    <p><strong>Email:</strong> ${resumeData.email}</p>
+                    <div class="about">
+                      <h3>About</h3>
+                      <p>${resumeData.about}</p>
+                    </div>
+                  </div>
+                  <div class="secondSection">
+                    <div class="experienceDisplay">
+                      <h2>Work Experience</h2>
+                      <p><strong>Company:</strong> ${resumeData.company}</p>
+                      <p><strong>Position:</strong> ${resumeData.position}</p>
+                      <p><strong>From:</strong> ${resumeData.startDate}</p>
+                      <p><strong>To:</strong> ${resumeData.endDate}</p>
+                      <p><strong>Description:</strong> ${resumeData.description}</p>
+                    </div>
+                    <section class="skills-container">
+                      <h2>Skills</h2>
+                      <ul class="skills-list">
+                        ${resumeData.skills.map((skill: string) => `<li>${skill}</li>`).join('')}  
+                      </ul>
+                    </section>
+                    <h2>Education</h2>
+                    <p><strong>Degree:</strong> ${resumeData.degree}</p>
+                    <p><strong>Year:</strong> ${resumeData.academicYear}</p>
+                    <p><strong>School/College:</strong> ${resumeData.school}</p>
+                  </div>
+                </div>
+            `;
+
+            ResumeDisplay.innerHTML = resumeHTML;
+        }
+    }
+});
+
+const GenerateButton = document.getElementById('generateButton') as HTMLButtonElement;
+const saveButton = document.getElementById('saveButton') as HTMLButtonElement;
+let generated = false;
+GenerateButton.addEventListener('click', function () {
+    if (!generated) {
+        GenerateButton.textContent = "Edit Resume";
+        saveButton.style.display = 'inline-block';
+        generated = true;
+        shareHeading.style.display = 'flex';
+        shareLink.style.display = 'block';
+        
+    }
+});
+
+saveButton.addEventListener('click', function () {
+    ResumeForm.style.display = 'none';
+    mainHeading.style.display = 'none';
+    downloadPDF.style.display = 'block';
+    alert('Resume Saved Successfully');
+});
+
+// download PDF
+downloadPDF.addEventListener('click', function () {
+    if (ResumeDisplay && ResumeDisplay.innerHTML) {
+        const opt = {
+            filename: 'resume.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        };
+
+        html2pdf().from(ResumeDisplay).set(opt).save();
+    }
+});
